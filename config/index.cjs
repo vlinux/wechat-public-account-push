70d98427d9a5c3fe70dcb3f2eaf6e0f62edf8c67
vlinux <73208462+vlinux@users.noreply.github.com>
2022-11-29 17:09:09 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -54,13 +54,12 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '0002',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      useTemplateId: '0002',
       id: '7606bc5d1ae1495cb9b6b16635fcc7de'
     }
   ],
